8ac812fe48fbe991c663c3cb015d88f2d28ae622
camryn williams <williams.camryn.e@gmail.com>
2021-03-26 11:16:03 -0400

    completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  document.addEventListener("submit", function(event){
+    event.preventDefault();
+    const task = document.querySelector("input[type = 'text']");
+    let listTask = document.createElement("li");
+    let taskText = document.createTextNode(task);
+    listTask.appendChild(taskText);                           
+    document.getElementById('tasks').appendChild(listTask);
+    
+  });
 });
+
